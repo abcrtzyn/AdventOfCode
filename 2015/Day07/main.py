408174e264cde21419c465de54cd716d69f18e39
Aidan Christensen <aidan_christensen@icloud.com>
2024-11-07 22:23:07 -0600

    2015.07

--- 2015/Day07/main.py
+++ 2015/Day07/main.py
@@ -1,62 +1,71 @@
 
 
-terms = {}
+terms_original = {}
 
-with open('Day07/inputpart2.txt') as f:
+#  parsing the terms
+with open('Day07/input.txt') as f:
     for line in f.readlines():
-        eq, term = [x.strip() for x in line.strip().split(' -> ')]
-        if eq.isnumeric():
-            terms[term] = int(eq)
+        expression, name = [x.strip() for x in line.strip().split(' -> ')]
+        if expression.isnumeric():
+            terms_original[name] = int(expression)
         else:
-            terms[term] = eq
+            terms_original[name] = expression
 
-def calculate(term):
-    if term.isnumeric():
-        return int(term)
-    eq = terms[term]
-    #print('current', term, '=', eq)
-    if type(eq) == int:
-        return eq
+# calculates the name given using term bank
+# calculates all dependicies and simplifies them in the bank
+# returns the integer result of the computation
+def calculate(name,terms):
+    if name.isnumeric():
+        # if it is a numeric string (usually second argument of a shift)
+        # return the int
+        return int(name)
+    # otherwise, get the expression
+    expression = terms[name]
+    
+    if type(expression) == int:
+        # if the expression is already an int, return that
+        return expression
     else:
         # string
-        eq = eq.split()
-        if len(eq) == 1:
-            # direct input
-            ans = calculate(eq[0])
-            #print(term,'=',ans)
-            terms[term] = ans
+        expression = expression.split()
+        if len(expression) == 1:
+            # direct input (a -> b)
+            ans = calculate(expression[0],terms)
+            terms[name] = ans
             return ans
-        elif len(eq) == 2:
-            if eq[0] == 'NOT':
-                ans = ~ calculate(eq[1])
-                #print(term,'=',ans)
-                terms[term] = ans
+        elif len(expression) == 2:
+            if expression[0] == 'NOT':
+                ans = ~ calculate(expression[1],terms)
+                terms[name] = ans
                 return ans
-        elif len(eq) == 3:
-            if len(eq) == 3:
-                if eq[1] == 'OR':
-                    ans = calculate(eq[0]) | calculate(eq[2])
-                    #print(term,'=',ans)
-                    terms[term] = ans
-                    return ans
-                elif eq[1] == 'AND':
-                    ans = calculate(eq[0]) & calculate(eq[2])
-                    #print(term,'=',ans)
-                    terms[term] = ans
-                    return ans
-                elif eq[1] == 'LSHIFT':
-                    ans = calculate(eq[0]) << calculate(eq[2])
-                    #print(term,'=',ans)
-                    terms[term] = ans
-                    return ans
-                elif eq[1] == 'RSHIFT':
-                    ans = calculate(eq[0]) >> calculate(eq[2])
-                    #print(term,'=',ans)
-                    terms[term] = ans
-                    return ans
+        elif len(expression) == 3:
+            if expression[1] == 'OR':
+                ans = calculate(expression[0], terms) | calculate(expression[2], terms)
+                terms[name] = ans
+                return ans
+            elif expression[1] == 'AND':
+                ans = calculate(expression[0], terms) & calculate(expression[2],terms)
+                terms[name] = ans
+                return ans
+            elif expression[1] == 'LSHIFT':
+                ans = calculate(expression[0],terms) << calculate(expression[2],terms)
+                terms[name] = ans
+                return ans
+            elif expression[1] == 'RSHIFT':
+                ans = calculate(expression[0],terms) >> calculate(expression[2],terms)
+                terms[name] = ans
+                return ans
                 
 
-    raise Exception(eq)
+    raise Exception('unknown expression',expression)
 
 
-print(calculate('a'))
+terms1 = terms_original.copy()
+
+part1 = calculate('a',terms1)
+print('Part 1:', part1)
+
+terms2 = terms_original.copy()
+# override term b
+terms2['b'] = part1
+print('Part 2:', calculate('a',terms2))
