c8b106619e746c7d96a399df68b1fd10f7fa9c52
Aidan Christensen <aidan_christensen@icloud.com>
2024-11-09 12:09:58 -0600

    2015.17 shorter python solution

--- 2015/Day17/main.py
+++ 2015/Day17/main.py
@@ -9,29 +9,17 @@ with open('Day17/input.txt') as f:
 import itertools
 
 ways = 0
-# trying all possible combinations
+# trying all possible combinations of all lengths, maybe not the last one
+# this is not efficient because there are ways to ignore various branches of the combinations trees
 for i in range(len(containers)):
-    for com in itertools.combinations(containers,i):
-        liquid = 150
-        for c in com:
-            liquid -= c
-            if liquid < 0:
-                break
-        if liquid == 0:
-            ways += 1
+    # isn't this just significantly smaller than the previous version I had
+    ways += sum((150-sum(com)==0) for com in itertools.combinations(containers,i))
+        
+print('Part 1:',ways)
+
+
+
+                                                                         # 4 is the minimum containers in my case
+ways  = sum((150-sum(com)==0) for com in itertools.combinations(containers,4))
     
-
-print(ways)
-
-ways = 0
-
-for com in itertools.combinations(containers,4):
-    liquid = 150
-    for c in com:
-        liquid -= c
-        if liquid < 0:
-            break
-    if liquid == 0:
-        ways += 1
-
-print(ways)
+print('Part 2:',ways)
