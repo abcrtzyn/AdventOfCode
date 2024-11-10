179c3fabf2bc335b783ffe9aad5d3adaa5217932
Aidan Christensen <aidan_christensen@icloud.com>
2024-11-09 20:59:45 -0600

    2016.04

--- 2016/Day04/main.py
+++ 2016/Day04/main.py
@@ -4,7 +4,10 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 with open('Day04/input.txt') as f:
     for line in f.readlines():
         line = line.strip()
+        # get the checksum
         checksum = line[line.find('['):][1:-1]
+        
+        # make a frequency table
         freq = {}
         number = 0
         for c in line:
@@ -20,23 +23,25 @@ with open('Day04/input.txt') as f:
                 freq[c] += 1
             else:
                 freq[c] = 1
-        
+        # create the sorted list of letters
         check = [x[0] for x in sorted(sorted(freq.items(),key=lambda x:x[0]),key=lambda x:x[1],reverse=True)[:5]]
-        #print(check)
-
+        
+        # check that it is correct
         for a,b in zip(check,checksum):
             if a != b:
                 break
         else:
             # this one is good
             score += number
+            # decode the name
             shift = number % 26
             shifted = str.maketrans(alphabet+'-',alphabet[shift:] + alphabet[:shift] +' ','0123456789')
             name = line[:line.find('[')].translate(shifted)
-            print(name,number)
+            if 'north' in name:
+                print(name)
+                print('Part 2:',number)
 
 print(score)
 
         
         
-
