d7f08821ea01903e81560f8fa3c27dc525043580
Aidan Christensen <aidan_christensen@icloud.com>
2024-12-02 21:37:20 -0600

    2017.05

--- 2017/Day05/main.py
+++ 2017/Day05/main.py
@@ -1,31 +1,29 @@
-PART2 = True
+
+
+def solve(numbers,part2: bool):
+    limit = len(numbers)
+    i = 0
+    jump_count = 0
+
+    while 0 <= i < limit:
+        jump = numbers[i]
+        # adjust numbers
+        if part2 and jump >= 3:
+            numbers[i] -= 1
+        else:
+            numbers[i] += 1
+        i += jump
+        jump_count += 1
+    
+    return jump_count
 
 
 # each line is a number, make a list
-
 numbers = []
-
 with open('Day05/input.txt') as f:
-    for x in f.readlines():
+    for x in f:
         numbers.append(int(x.strip()))
 
-#print(numbers)
 
-
-limit = len(numbers)
-i = 0
-jump_count = 0
-
-while 0 <= i < limit:
-    jump = numbers[i]
-    if PART2 and jump >= 3:
-        numbers[i] -= 1
-    else:
-        numbers[i] += 1
-    i += jump
-    jump_count += 1
-
-for x in numbers[0:500]:
-    print(x)
-
-print('steps:', jump_count)
+print('Part 1:',solve(numbers.copy(),False))
+print('Part 2:',solve(numbers.copy(),True))
