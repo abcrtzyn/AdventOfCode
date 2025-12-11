060a4c515fdeace247d154ff018a6dfd22678702
Aidan Christensen <aidan_christensen@icloud.com>
2025-12-11 11:10:44 -0600

    prep for 2025, modify make year script

--- make_year.py
+++ make_year.py
@@ -1,13 +1,17 @@
 # this script makes a new directory with the year
 # and then adds 25 directories for days 1 through 25
 
-
-YEAR = 2024
+YEAR = 2025
 
 import os
 
 os.mkdir(f'year{YEAR}')
 os.chdir(f'year{YEAR}')
-for i in range(1,26):
+if YEAR <= 2024:
+    max_day = 25
+else:
+    max_day = 12
+
+for i in range(1,max_day+1):
     # print(f'Day{i:02d}')
     os.mkdir(f'Day{i:02d}')
